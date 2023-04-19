95bccdbf209c2d56f62a1560ce02cd68d5feef5f
DanielPerezGonz <116338863+DanielPerezGonz@users.noreply.github.com>
2023-04-19 18:49:27 +0200

    cositas

--- index.js
+++ index.js
@@ -1,186 +1,312 @@
-#!/usr/bin/node
+#!/bin/node
+
+//#!/usr/bin/env node
 
 const http = require('http');
 const { MongoClient } = require('mongodb');
 const fs = require('fs');
+const qs = require('querystring');
 
-//connection URL
-cont url = 'mongodb://127.0.0.1:27017';
+//Connection URL
+const url = 'mongodb://127.0.0.1:27017';
+
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'myProject';
+
+const dbName = 'enbuscadeabascal';
 
 let db;
+let collection;
 
 async function db_connect() {
+
   // Use connect method to connect to the server
+
   await client.connect();
+
   console.log('Connected successfully to server');
+
   db = client.db(dbName);
+
   //const collection = db.collection('documents');
 
   return 'Conectando a la base de datos MongoDB';
+
 }
 
 db_connect()
-	.then(info => console.log(info))
-	.catch(msg => console.error(msg));
+
+	.then(console.log)
+	.catch(console.error);
 
 
-function send_characters(response){
+function send_characters (response){
 
-	let collection = db.collection('characters');
-	
-	collection({}).toArray().then(query => {
-		let names = [];
+	collection = db.collection('characters');
+
+	collection.find({}).toArray().then(characters => {
+
+		let characters_name=[];
+
+		for (let i = 0; i < characters.length; i++){
 		
-		for (let i = 0; i < characters.length;i++){
-			names.push( characters[i].name );
+			characters_name.push( characters[i].name );
 		}
+
+		console.log(names);
 		
-		response.write(JSON.stringify(query));
+		response.write(JSON.stringify(characters_name));
 		response.end();
-		
+
 	});
+	
 }
 
 function send_age(response, url){
 
-	if (url.length < 3){
-		response.write("ERROR: Edad Erronea");
+	if(url.length < 3) {
+	
+		response.write("ERROR: Edad errónea");
 		response.end();
+		
 		return;
+	} 
+
+collection = db.collection('characters');
+
+collection.find({ "name": url[2] }).project({ _id: 0, age: 1}).toArray().then(character => {
+	
+	console.log(character);
+
+	if (character.length == 0){
+		
+		response.write("ERROR: Edad errónea");
+		response.end();
+
+		return;
+
 	}
 
-	let collection = db.collection('characters');
+	response.write(JSON.stringify(character[0]));
+	response.end();
 
-	collection.findOne({"name":url[2]},{"age"}).project({_id:0,age:1}).toArray().then(character => {
-		
-		if (character.length == 0){
-			response.write("ERROR: Introduce un personaje");
-			response.end();
-			return;
-		}
-		
-		response.write(JSON.stringify(character[0]));
-		response.end();
-		
 	});
 }
 
-function send_character_items(response, url){
-	let name = url[2].trim;
+function send_character_items ( response, url){
+
+	let name = url[2].trim();
+
 	if (name == ""){
-		response.write("ERROR: URL mal formada");
+	
+		response.write("error, URL not accepted");
 		response.end();
-		
+
 		return;
 	}
-	
-	let collection = db.collection('characters');
-	collection.find({"name":name}).toArray().then(character => {
-		if (character.length != 1){
-			response.write("ERROR: el personaje "+name+" no existe");
-			response.end();
-			
-			return;
-		}
+
+	let collection = db.collection("characters");
+	collection.find({"name": name}).project({_id: 0, id_character: 1}).toArray().then(characterID =>{
 		
-		let id = character[0].id_character;
-		let collection = db.collection('characters_items');
-		collection.find ({"id_character":id}).toArray().then(ids => {
-			if (ids.length == 0){
+		console.log(characterID);
+
+		if ( characterID.length != 1){
+			
+			response.write("error, el personaje " + name + " no existe");
+			response.end();
+
+			return;
+		
+		}
+	
+		let collection = db.collection("character_items");
+		collection.find({"id_character": characterID[0].id_character}).project({_id: 0, id_item1}).toArray().then(itemsID =>{
+			
+			console.log(itemsID);
+
+			if(ids.length == 0){
+				
 				response.write("[]");
 				response.end();
-				
+
 				return;
 			}
 			
-		}
-	})
+			let collection = db.collection("item");
+			let ids= [];
+
+			itemsID.forEache(element => ids.push(element.id_items));
+			
+			collection.find({"id_item": {$in:ids} }).project({_id: 0, item: 1}).toArray().then(items_name => {
+				
+				let names = [];
+
+				items_name.forEach( item => names.push(item.item));
+
+				response.write(JSON.stringify(names));
+				response.end();
+
+				return;
+			});
+		});
+	});
 }
 
-function send_items(response, url){
-	if (url.length >= 3){
+function send_items(response, url_split){
+	
+	if(url.length >= 3){
+
 		send_character_items (response, url);
 		
 		return;
 	}
-	
-	let collection = db.collection('items');
-	
-	collection({}).toArray().then(query => {
-		let names = [];
-		
-		for (let i = 0; i < items.length;i++){
-			names.push( items[i].item );
-		}
-		
-		response.write(JSON.stringify(query));
+
+	collection = db.collection('items');
+
+	collection.find({}).toArray().then(items => {
+
+	console.log(items);
+
+	let items_name = [];
+
+	for ( let i = 0; i < items.length; i++){
+		items_name.push(items[i].item);
+	}
+
+		response.write(JSON.stringify(items_name));
 		response.end();
-		
 	});
 }
 
-function insert_character(request, response){
-	if (request.method != "POSI"){
-		response.write("ERROR: Formulario no encontrado");
+function send_weapons (response){
+
+	collection = db.collection('weapons');
+
+	collection.find({}).toArray().then(weapons => {
+		
+		console.log(weapons);
+
+		let weapons_name = [];
+
+		for (let i = 0; i < weapons.length; i++){
+			weapons_name.push(weapons[i].weapon);
+		}
+
+		response.write(JSON.stringify(weapons_name));
 		response.end();
+	});
+}
+
+
+
+function insert_character (request, response){
+	
+	if (request.method != "POST"){
+		
+		response.write("ERROR: Formulario no enviado");
+		response.end();
+
 		return;
 	}
 
-	request.on('data', function(character_data){
-		data += character_chunk;
-	});
+	let data = "";
 
-	request.on('end'. function(){
+	request.on('data', character_chunk => data += character_chunk);
+	request.on('end', () => {
+		
 		console.log(data);
 
+		let info = qs.parse(data);
+
+		console.log(info);
+
+		let collection = db.collection("characters");
+
+		if (info.name == undefined){
+		
+			response.write("ERROR: Nombre no definido");
+			response.end();
+
+			return;
+		}
+
+		if (info.age == undefined){
+			
+			response.write("ERROR: Edad no definida");
+			response.end();
+
+			return;
+		}
+
+		let insert_info = {
+			
+			name: info.name,
+			age: parseInt(info.age)
+		};
+
+		collection.insertOne(insert_info);
+
+		response.write("Nuevo personaje insertado correctamente: " + insert_info.name);
 		response.end();
 	});
 }
 
-let http_server = http.createServer(function(request, result)){
-	if (request.url == "/favicon.ico"){
+let http_server = http.createServer(function(request, response){
+	
+	if(request.url == "/favicon.ico"){
 		return;
-	}
+	}	
+
+	console.log("Se conecto");
 
 	let url = request.url.split("/");
 
-	switch(url[1]){
-		case "characters":
-			send_characters(response);
-			break;
-		case "age":
-			send_age(response, url);
-			break;
-		case "items":
-			send_items(response, url);
-			break;
-		case "character_form":
-			insert_character(request, response);
-			break;
-		default:
-			fs.readFile("index.html", function(err, data){ 
-				if (err){
-					console.error(err);
-					response.writeHead(404, {'Content-Type':'text/html'});
-					response.write ("Error 404: el archivo no esta en esta castillo");
-					response.end();
+	switch (url[1]) {
+	
+	case "age":
+		send_age(response, url);
+		break;
 
-					return;
-				}
+	case "characters":
+		send_characters(response);
+		break;
+	
+	case "items":
+		send_items(response, url);
+		break;
 
-				response.writeHead(200, {'Content-Type':'text/html'});
+	case "weapons":
+		send_weapons(response);
+		break;
 
-				response.write("Pagina principal");
-				response.end();
-			});
+	case "character_form":
+		insert_character(request, response);
+		break;
+
+	default:
+
+		fs.readFile("index.html", function(err, data){
+		
+		if (err){
+			
+			console.error(err);
+			
+			response.writeHead(404, {"Content-Type":"text/html"});
+			response.write("Error 404: el archivo no está");
+			response.end();
+
+			return;
+	
+		}
+
+		response.writeHead(200, {"Content-Type":"text/html"});
+		response.write(data);
+
+		response.end();
+	
+		});
+
 	}
 
-	console.log(request.url);
-
-});
-
-http_server.listen(6969);
+}).listen(7887);
